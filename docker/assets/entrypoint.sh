09fd0361b558d54570fef29bc2f174c112c4cfe9
Tommy Bolger <tommy.bolger@gmail.com>
2024-10-04 01:52:34 -0700

    Refactored the Docker environment to enable https, nginx proxying, custom local domains for the backend and frontend, and more distinct service names.

---
+++ docker/assets/entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+cd /var/www/frontend
+/usr/local/bin/npm install
+npm run dev
---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo "Copying the .env file for development..."
+cp .env.example .env
+
+echo "Running composer install..."
+docker-compose exec wild-alaskan-backend composer install
+
+echo "Generating an application key..."
+docker-compose exec wild-alaskan-backend php artisan key:generate
+
+echo "Restarting environment..."
+docker-compose down
+docker volume rm wild-alaskan-test_wild-alaskan-mysql
+docker-compose up -d
+
+sleep 10
+
+echo "Running migrations..."
+docker-compose exec wild-alaskan-backend php artisan migrate
+
+echo "Running seeders..."
+docker-compose exec wild-alaskan-backend php artisan db:seed --class=RecipesSeeder
+
+echo "Clearing cache..."
+docker-compose exec wild-alaskan-backend php artisan cache:clear
+docker-compose exec wild-alaskan-backend php artisan config:clear
+docker-compose exec wild-alaskan-backend php artisan route:clear
+docker-compose exec wild-alaskan-backend php artisan view:clear
+
+echo "Linking storage..."
+docker-compose exec wild-alaskan-backend php artisan storage:link
